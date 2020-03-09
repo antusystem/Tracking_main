11cdb106883b6b35caa667eb00d827a08ab35b3b
antusystem <aleantunes95@gmail.com>
2020-03-09 14:10:46 -0400

    First Commit
    Ya compila, falta probar. Deberia de medir temperatura y mandar dos
    mensajes ciclicamente

---
+++ main/include/ClienteTCP.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_INCLUDE_CLIENTETCP_H_
+#define MAIN_INCLUDE_CLIENTETCP_H_
+
+// Funciones prototipo --------------------------
+void iniciar_wifi(void);
+void TareaClienteTCP(void *P);
+
+#endif /* MAIN_INCLUDE_CLIENTETCP_H_ */
---
+++ main/include/dht.h
@@ -0,0 +1,21 @@
+#ifndef __DHT_H__
+#define __DHT_H__
+#include "stdint.h"
+
+struct form_home{
+	char Humedad1[6];
+	char Temperatura1[6];
+	uint16_t Humedad2;
+	uint16_t Temperatura2;
+	char Datos_Sensor[60];
+	float Humedad3;
+	float Temperatura3;
+};
+
+
+struct form_home form1;
+
+
+void TareaDHT(void *P);
+
+#endif

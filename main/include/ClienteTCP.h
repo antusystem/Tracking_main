ff99b0989747e10f8c0c05d83aa1d4e8ec3db7f4
antusystem <aleantunes95@gmail.com>
2020-03-18 15:29:48 -0400

    Borrar TCPClient.h
    Se borro TCPClient.h ya que no era necesario porque no se piensa
    conectar a wifi

--- main/include/ClienteTCP.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_INCLUDE_CLIENTETCP_H_
-#define MAIN_INCLUDE_CLIENTETCP_H_
-
-// Funciones prototipo --------------------------
-void iniciar_wifi(void);
-void TareaClienteTCP(void *P);
-
-#endif /* MAIN_INCLUDE_CLIENTETCP_H_ */
